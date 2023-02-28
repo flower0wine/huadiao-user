d97004ca164522ce131087d0771a21740e855cc4
flowerwine <2914518090@qq.com>
2023-02-28 11:10:43 +0800

    初次提交

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    // 域名子路径,
+    publicPath: process.env.NODE_ENV === 'production' ? '/huadiao-user/' : '/',
+    lintOnSave: false,
+    outputDir: "huadiao-user",
+    assetsDir: "static",
+    pages: {
+        index: {
+            // 主页入口
+            entry: "./src/pages/index/index.js",
+            // 主页
+            template: './src/pages/index/index.html',
+            // 打包后的html文件名称
+            filename: "index.html",
+            // 主页标题
+            title: '花凋~~不一样的世界'
+        },
+    }
 })
